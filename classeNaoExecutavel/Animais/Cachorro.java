ce595894514e2ec09d035ab051ee2c4f1e0265be
Rafael Melchior de Oliveira <118372879+Rafuel05@users.noreply.github.com>
2023-11-15 23:57:50 +0000

    Encapsulamento

--- classeNaoExecutavel/Animais/Cachorro.java
+++ classeNaoExecutavel/Animais/Cachorro.java
@@ -1,15 +1,70 @@
 package classeNaoExecutavel.Animais;
 
 public class Cachorro {
+//atributos
+    private String name;
+    private String cor;
+    private int altura;
+    private double peso;
+    private int tamanhoDoRabo;
+    private String estadoDeEspirito;
+//------------------------------------
 
-    public String name;
-    public String cor;
-    public int altura;
-    public double peso;
-    public int tamanhoDoRabo;
-    public String estadoDeEspirito;
+//construtores
+    public Cachorro ( ){};
+    public Cachorro(String name, String cor, int altura, double peso, int tamanhoDoRabo, String estadoDeEspirito){
+        this.name = name;
+        this.cor = cor;
+        this.altura = altura;
+        this.peso = peso;
+        this.tamanhoDoRabo = tamanhoDoRabo;
+    }
+//------------------------------
+//metodos
+    public String getName(){
+        return this.name;
+    }
 
+    public String getCor(){
+        return this.cor;
+    }
 
+    public String getEstadoDeEspirito(){
+        return this.estadoDeEspirito;
+    }
+
+    public int getAltura(){
+        return this.altura;
+    }
+
+    public double getPeso(){
+        return this.peso;
+    }
+
+    public int getTamanhoDoRabo(){
+        return this.tamanhoDoRabo;
+    }
+
+    public void setCor(String cor){
+        this.cor = cor;
+    }
+
+    public void setAltura(int altura){
+        this.altura = altura;
+    }
+
+    public void setTamanhoDoRabo(int tamanhoDoRabo){
+        this.tamanhoDoRabo = tamanhoDoRabo;
+    }
+
+    public void setPeso(double peso){
+        this.peso = peso;
+    }
+
+    public void setName(String nome){
+        this.name = nome;
+    }
+    
     public void comer () {}
 
     public void latir () {
@@ -18,15 +73,24 @@ public class Cachorro {
     public String pegar(){
         return "Bolinha";
     }
-
+//---------------------------------------
     public String interagir(String acao){
-        if (acao.equals("carinho")){
-            this.estadoDeEspirito = "feliz";
-        }else if (acao.equals("vai dormir")) {
-            this.estadoDeEspirito = "triste"; 
-        }
-        else { 
+        switch (acao) {
+            case "carinho" :
+                this.estadoDeEspirito = "feliz";
+                break;
+
+            case "vai dormir" :
+            this.estadoDeEspirito = "triste";
+            break;
+
+            case "pisar na patinha" :
+            this.estadoDeEspirito = "bravo";
+            break;
+
+            default:
             this.estadoDeEspirito = "di boa";
+            break;
         }
         return estadoDeEspirito;
     }
--- classeNaoExecutavel/Main.java
+++ classeNaoExecutavel/Main.java
@@ -5,19 +5,20 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Cachorro cachorro_1 = new Cachorro();
+        Cachorro cachorro_1 = new Cachorro("Tobby","marrom",25,5.5,2,"di boa");
 
-        cachorro_1.name = "Tobby";
-        cachorro_1.cor = "marrom";
-        cachorro_1.altura = 25;
-        cachorro_1.peso = 5.5;
-        cachorro_1.tamanhoDoRabo = 2;
+        //cachorro_1.setName("Tobby");
+        //cachorro_1.setCor("marrom");
+        //cachorro_1.setAltura(25);
+        //cachorro_1.setPeso(5.5);
+        //cachorro_1.setTamanhoDoRabo(2);
 
         
         cachorro_1.latir();
         System.out.println("O cachorro achou a " + cachorro_1.pegar());
         System.out.println("Apos o carinho o cachorro esta " + cachorro_1.interagir("carinho"));
         System.out.println("Apos mandar o cachorro ir dormir ele ficou " + cachorro_1.interagir("vai dormir"));
-
+        System.out.println("Apos pisar na patinha o cachorro ficou " + cachorro_1.interagir("pisar na patinha") );
+        System.out.println("Vc nao pediu nada ao cachorro ele esta " + cachorro_1.interagir("nada") );
     }
 }
